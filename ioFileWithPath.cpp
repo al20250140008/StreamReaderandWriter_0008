24ad5cdc8f195b33b64648e9c23b65a6effca7b1
al20250140008 <alf80169@gmail.com>
2026-06-01 10:28:02 +0700

    Menambahkan fungsi main dan logika untuk menangkap nama file dari input pengguna

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,3 +3,13 @@
 #include <string>
 using namespace std;
 
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    
